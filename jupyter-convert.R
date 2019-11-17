9f0a2c751ec5274adbdd04e963cafcde77df8e69
Joaq <joaquin.almirante@gmail.com>
2019-11-18 00:33:02 +0800

    Edit jupyter convert file

--- jupyter-convert.R
+++ jupyter-convert.R
@@ -1,6 +1,7 @@
 # Convert Joaq's Jupyter notebook to R Markdown
 #install.packages("rmarkdown")
 library(rmarkdown)
-input <- "20191117_EDA.ipynb"
+input <- "20191118_JOAQ_BART.ipynb"
 
-R::convert_ipynb(input, output = xfun::with_ext(input, "Rmd"))
+rmarkdown:::convert_ipynb(input, output = xfun::with_ext(input, "Rmd"))
+
